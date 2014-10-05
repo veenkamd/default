c38325cb7d2093ee60d22512d9b06c31e01cf170
Robert Smith <smithro3@mail.gvsu.edu>
2014-10-05 16:04:29 -0400

    Corrected SearchScreen.java and activty_search.xml to match code review specifications. Changed background color of Collections screen to match other screens.

--- app/src/main/java/com/merqurius/search/SearchScreen.java
+++ app/src/main/java/com/merqurius/search/SearchScreen.java
@@ -9,23 +9,11 @@ import com.merqurius.R;
 
 public class SearchScreen extends Activity {
 
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_activity);
+        setContentView(R.layout.activity_search);
     }
-
-   /* //Called when the user clicks the send button
-    public void sendMessage(View view){
-        //do something in response to the button
-        //Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.title_text);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,9 +24,6 @@ public class SearchScreen extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
