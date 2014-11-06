01211b872f1942ac9f83e3adeb6091c4f04625e9
Morgan Heyboer <morganheyboer@gmail.com>
2014-11-05 21:53:12 -0500

    Moved all SQL strings from MySQLiteHelper to Database class

--- app/src/main/java/com/merqurius/Database.java
+++ app/src/main/java/com/merqurius/Database.java
@@ -1,8 +1,5 @@
 package com.merqurius;
 
-/**
- * Created by Dan on 19/10/2014.
- */
 public final class Database {
     //Book Table
     public static final String BOOK_TBL    = "book";
@@ -21,6 +18,26 @@ public final class Database {
     public static final String COLLECTION_TBL = "collection";
     public static final String COLLECTION_NAME = "collection_name";
     public static final String COLLECTION_ID   = "collection_id";
+
+    //Create Statements
+    public static final String CREATE_BOOK = "create table " + BOOK_TBL + "("
+            + ISBN + " varchar(13) primary key, "
+            + STATUS + " varchar(20) not null, "
+            + TITLE + " varchar(200) not null, "
+            + GENRE + " varchar(50) not null, "
+            + LOANED_TO + " varchar(50), "
+            + NUM_PAGES + " integer, "
+            + AUTHOR + " varchar(50) not null, "
+            + COLLECTION + " integer not null, "
+            + RATING + " integer, "
+            + DESCRIPTION + " varchar(300));";
+    public static final String CREATE_COLLECTION = "create table " + COLLECTION_TBL + "("
+            + COLLECTION_ID + " integer primary key, "
+            + COLLECTION_NAME + " varchar(25));";
+
+    //Drop Statements
+    public static final String DROP_BOOK = "DROP TABLE IF EXISTS " + BOOK_TBL;
+    public static final String DROP_COLLECTION = "DROP TABLE IF EXISTS " + COLLECTION_TBL;
 
     //Queries
     public static final String SELECT_ALL_COLLECTIONS = "SELECT * FROM collection";
--- app/src/main/java/com/merqurius/MySQLiteHelper.java
+++ app/src/main/java/com/merqurius/MySQLiteHelper.java
@@ -9,43 +9,8 @@ import android.util.Log;
 
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
-    //Book Table
-    public static final String BOOK_TBL    = "book";
-    public static final String TITLE       = "title";
-    public static final String AUTHOR      = "author";
-    public static final String GENRE       = "genre";
-    public static final String ISBN        = "isbn";
-    public static final String DESCRIPTION = "description";
-    public static final String STATUS      = "status";
-    public static final String LOANED_TO   = "loaned_to";
-    public static final String NUM_PAGES   = "num_pages";
-    public static final String COLLECTION  = "collection";
-    public static final String RATING      = "rating";
-
-    //Collection Table
-    public static final String COLLECTION_TBL = "collection";
-    public static final String COLLECTION_NAME = "collection_name";
-    public static final String COLLECTION_ID   = "collection_id";
-
     private static final String DATABASE_NAME = "BookManager.db";
     private static final int DATABASE_VERSION = 1;
-
-    //Create SQL statements
-    public static final String CREATE_BOOK = "create table " + BOOK_TBL + "("
-            + ISBN + " varchar(13) primary key, "
-            + STATUS + " varchar(20) not null, "
-            + TITLE + " varchar(200) not null, "
-            + GENRE + " varchar(50) not null, "
-            + LOANED_TO + " varchar(50), "
-            + NUM_PAGES + " integer, "
-            + AUTHOR + " varchar(50) not null, "
-            + COLLECTION + " integer not null, "
-            + RATING + " integer, "
-            + DESCRIPTION + " varchar(300));";
-
-    public static final String CREATE_COLLECTION = "create table " + COLLECTION_TBL + "("
-            + COLLECTION_ID + " integer primary key, "
-            + COLLECTION_NAME + " varchar(25));";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -53,8 +18,8 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(CREATE_BOOK);
-        database.execSQL(CREATE_COLLECTION);
+        database.execSQL(Database.CREATE_BOOK);
+        database.execSQL(Database.CREATE_COLLECTION);
     }
 
     @Override
@@ -62,17 +27,17 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
         Log.w(MySQLiteHelper.class.getName(),
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS " + BOOK_TBL);
-        db.execSQL("DROP TABLE IF EXISTS " + COLLECTION_TBL);
+        db.execSQL(Database.DROP_BOOK);
+        db.execSQL(Database.DROP_COLLECTION);
         onCreate(db);
     }
 
     public void insertCollection(String collectionName) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
-        cv.put(COLLECTION_NAME, collectionName);
+        cv.put(Database.COLLECTION_NAME, collectionName);
         //cv.put(COLLECTION_ID, "");
-        db.insertOrThrow(COLLECTION_TBL, null, cv);
+        db.insertOrThrow(Database.COLLECTION_TBL, null, cv);
         db.close();
     }
 
