0b8610b61b1d11b21f1bdd606918efa434dddf68
Morgan Heyboer <morganheyboer@gmail.com>
2014-12-07 14:40:00 -0500

    Adding to ZXing feature. Gives an error after the book is scanned in the Barcode Scanner and then returns to the home screen of the app.

--- app/src/main/java/com/merqurius/search/SearchScanScreen.java
+++ app/src/main/java/com/merqurius/search/SearchScanScreen.java
@@ -5,12 +5,16 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.merqurius.R;
 import com.google.zxing.integration.android.*;
+import com.merqurius.home.HomeScreen;
+
+import java.util.concurrent.ExecutionException;
 
 public class SearchScanScreen extends Activity {
     final Context context = this;
@@ -54,28 +58,55 @@ public class SearchScanScreen extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /*public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (result != null) {
-            String contents = result.getContents();
-            if (contents != null) {
-
-            } else {
-
-            }
-        }
-    }*/
     public void onActivityResult(int requestCode, int resultCode, Intent intent){
-        String contents;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        String barcodeScan;
+        View v = this.findViewById(R.id.buttonScanBook);
         if (resultCode == Activity.RESULT_OK){
-            contents = intent.getStringExtra("SCAN_RESULT") + " " + intent.getStringExtra("SCAN_RESULT_FORMAT");
+             barcodeScan = intent.getStringExtra("SCAN_RESULT");
+             String q = buildQuery(barcodeScan);
+             String r = fetchResults(q);
+             if(! r.equals("Unable to Connect")) {
+                Intent searchResultsIntent = new Intent(v.getContext(), SearchResultsScreen.class);
+                searchResultsIntent.putExtra("query", q);
+                searchResultsIntent.putExtra("response", r);
+                Log.d(getClass().getName(), "Forwarding to results screen.");
+                startActivityForResult(searchResultsIntent, 0);
+             } else {
+                builder.setMessage(r);
+                builder.show();
+             }
         } else {
-            contents = "This did not work";
+            builder.setMessage("The scan did not work. Please try again.");
+            builder.show();
+            Intent homeIntent = new Intent(v.getContext(), HomeScreen.class);
+            startActivityForResult(homeIntent, 0);
         }
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(contents);
-        builder.show();
     }
 
+    private String buildQuery(String s) {
+        String query = "https://www.googleapis.com/books/v1/volumes?q=";
+        if(s.length() > 0){
+            s = s.replaceAll(" ", ",");
+            query += "isbn:" + s;
+        }
+        query += "&key=AIzaSyBWUqhTT8y4aC9hyFgjenA3lhqi1cnV0R0&fields=items(volumeInfo"
+                + "/title,volumeInfo/authors,volumeInfo/industryIdentifiers)";
+
+        return query;
+    }
+
+    private String fetchResults(String query) {
+        try {
+            return new SearchInet(this).execute(query).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return "Unable to Connect";
+    }
 }
